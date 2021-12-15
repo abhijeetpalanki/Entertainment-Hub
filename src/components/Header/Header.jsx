ab601dba8fa405b2222c7e68b1030f86a56185ab
Abhijeet Palanki <abhijeet.palanki@tylertech.com>
2021-12-14 21:20:02 -0500

    entertainment-hub application

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import './Header.css';
+
+const Header = () => { return <span onClick={() => window.scrollTo(0,0)} className='header'>🎬 Entertainment Hub 🎥</span> }
+
+export default Header
